c6f1718f216e2cf65bde334e62ce9ab3ef393309
George Paul <georgepaul@Georges-MacBook-Air.local>
2026-01-29 04:48:54 +0530

    changed uuit generator

--- src/lib/todolists.svelte.ts
+++ src/lib/todolists.svelte.ts
@@ -1,16 +1,16 @@
 import type { TodoList } from './types';
 import { getRandomIcon } from '$lib/icon_generator/random_icon';
-// import type { IconName } from './icons';
+import { v4 as uuidv4 } from 'uuid';
 
 export const lists = $state<TodoList[]>([
 	{
-		id: crypto.randomUUID(),
+		id: uuidv4(),
 		name: 'Personal',
 		icon: getRandomIcon(),
 		theme: 'blue',
 		description: 'Personal tasks and reminders',
 		todos: [
-			{ id: crypto.randomUUID(), text: 'Buy milk', done: false }
+			{ id: uuidv4(), text: 'Buy milk', done: false }
 		]
 	}
 ]);
